7037ae69f97d053b7b6a420772178b3604fdb5c1
nabilcanan <nabiicanan78@gmail.com>
2023-03-26 12:55:03 -0400

    final commit

---
+++ Assets/MOVESKELETON.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOVESKELETON : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+            anim.SetTrigger("WALK");
+        if (Input.GetKeyDown(KeyCode.W))
+            anim.SetTrigger("RUN");
+        if (Input.GetKeyDown(KeyCode.W))
+            anim.SetTrigger("DEAD");
+
+    }
+}
